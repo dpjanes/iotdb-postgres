e0f430c7ff1ca8a782251ed9b856b38500e1e471
David Janes <david@consensas.com>
2018-01-25 15:25:00 -0500

    create table from command line

---
+++ bin/postgres-create-table.js
@@ -0,0 +1,32 @@
+/**
+ *  bin/postgres-create-table
+ *
+ *  David Janes
+ *  IOTDB
+ *  2018-01-25
+ *
+ *  Copyright [2013-2018] [David P. Janes]
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict";
+
+const _ = require("iotdb-helpers");
+const fs = require("iotdb-fs")
+
+const assert = require("assert");
+const path = require("path");
+
+const postgres = require("..")
+
