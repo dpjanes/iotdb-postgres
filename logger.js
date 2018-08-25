60c284d6b859531d74f2bd8d0281f7bc48b1ba30
David Janes <david@consensas.com>
2018-08-25 16:27:35 -0400

    add a logger function

---
+++ logger.js
@@ -0,0 +1,27 @@
+/*
+ *  logger.js
+ *
+ *  David Janes
+ *  IOTDB.org
+ *  2018-08-25
+ *
+ *  Copyright (2013-2018) David Janes
+ */
+
+"use strict"
+
+const _ = require("iotdb-helpers")
+
+const path = require("path")
+
+const _root = path.dirname(path.dirname(__filename));
+
+const logger = (source) => _.logger.make({
+    name: "iotdb-postgres",
+    source: source.substring(_root.length + 1).replace(/[.]js$/, ""),
+})
+
+/**
+ *  API
+ */
+module.exports = logger;
